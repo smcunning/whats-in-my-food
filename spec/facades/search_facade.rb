280f5569ad2f2a3611602a11587516a93fce4aa5
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 10:46:23 -0700

    Add testing for new facade methods

--- spec/facades/search_facade.rb
+++ spec/facades/search_facade.rb
@@ -5,7 +5,24 @@ describe 'Search Facade' do
     foods = SearchFacade.food_list("Sweet Potatoes", 10)
 
     expect(foods).to be_a(Hash)
-    expect(foods.size).to eq(10)
-    expect(food.first).to be_an_instance_of(Food)
+    expect(foods).to have_key :foods
+    expect(foods[:foods].first).to have_key :gtinUpc
+    expect(foods[:foods].first).to have_key :description
+    expect(foods[:foods].first).to have_key :brandOwner
+    expect(foods[:foods].first).to have_key :ingredients
+  end
+
+  it 'can create food objects' do
+    foods = SearchFacade.food_list("Sweet Potatoes", 10)
+    food_objects = SearchFacade.create_food_objects(foods)
+
+    expect(food_objects.first).to be_an_instance_of(Food)
+  end
+
+  it 'can get total_results' do
+    foods = SearchFacade.food_list("Sweet Potatoes", 10)
+    total_results = SearchFacade.total_results(foods)
+
+    expect(total_results).to be_an Integer
   end
 end
