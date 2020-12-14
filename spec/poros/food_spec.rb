61794d1da5984d3eb1f32a868ced922f10e8bb20
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 10:07:06 -0700

    Add spec for food poro

---
+++ spec/poros/food_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Food PORO' do
+  it "has attributes" do
+    food_data = {
+            "gtinUpc": 12345,
+            "description": "Apple",
+            "brandOwner": "Dole",
+            "ingredients": "Apple"
+           }
+
+    food = Food.new(food_data)
+    expect(food.description).to eq(food_data[:description])
+    expect(food.brand).to eq(food_data[:brandOwner])
+    expect(food.ingredients).to eq(food_data[:ingredients])
+  end
+end
