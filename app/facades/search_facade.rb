942d41c5027218aeb82efcb3a9179e04535e7a06
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 09:49:12 -0700

    Update facade to call on foods key in map block - small update to index view to call on correct brand method

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,8 +1,8 @@
 class SearchFacade
   def self.food_list(search_term, result_quantity)
     foods = FoodService.food_list(search_term, result_quantity)
-    foods.map do |food|
+    foods[:foods].map do |food|
       Food.new(food)
-    end 
+    end
   end
 end
