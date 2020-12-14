90616354b15c8a6e9b39f6d31bbe0f3550fecec6
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 09:36:30 -0700

    Begin refactor into facade/poros/services

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,16 +1,19 @@
 class FoodsController < ApplicationController
   def index
-    conn = Faraday.new(url: 'https://api.nal.usda.gov/') do |f|
-      f.params[:api_key] = ENV['FOODS_API_KEY']
-    end
 
-    results = conn.get('/fdc/v1/foods/search?') do |req|
-      req.params[:query] = params[:q]
-      req.params[:pageSize] = 10
-    end
+    @foods = SearchFacade.new(params[:q]) 
 
-    food_list = JSON.parse(results.body, symbolize_names: true)
-
-    @foods = food_list[:foods]
+    # conn = Faraday.new(url: 'https://api.nal.usda.gov/') do |f|
+    #   f.params[:api_key] = ENV['FOODS_API_KEY']
+    # end
+    #
+    # results = conn.get('/fdc/v1/foods/search?') do |req|
+    #   req.params[:query] = params[:q]
+    #   req.params[:pageSize] = 10
+    # end
+    #
+    # food_list = JSON.parse(results.body, symbolize_names: true)
+    #
+    # @foods = food_list[:foods]
   end
 end
