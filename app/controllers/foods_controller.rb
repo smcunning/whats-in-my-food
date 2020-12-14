d7bcff42d65a6eb1099fe7d246edc4efe8dc3d69
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 09:29:53 -0700

    WIP - working call to API

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,17 @@
 class FoodsController < ApplicationController
   def index
-  end 
+    conn = Faraday.new(url: 'https://api.nal.usda.gov/') do |f|
+      f.params[:api_key] = ENV['FOODS_API_KEY']
+    end
+
+    results = conn.get('/fdc/v1/foods/search?') do |req|
+      req.params[:query] = params[:q]
+      req.params[:pageSize] = 10
+    end
+
+    food_list = JSON.parse(results.body, symbolize_names: true)
+
+    @foods = food_list[:foods]
+    require "pry"; binding.pry
+  end
 end
